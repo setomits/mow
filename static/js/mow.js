5e0c2b000eb16463b425a7f42243bc99f70fd6b9
Mitsuhiro SETOGUCHI <setomits@gmail.com>
2012-03-28 23:54:56 +0900

    Fixed element specification

--- static/js/mow.js
+++ static/js/mow.js
@@ -15,7 +15,7 @@ var MOW = {
             this.setCommentForm();
 
             if (location.hash) {
-                $(location.hash).animate(
+                $('#c' + location.hash.substr(1)).animate(
                     {backgroundColor: 'white'}, 1500,
                     function() {
                         $(this).animate(
