21bbea9fd3a5727b0db157d2536198bdf4a24842
Mitsuhiro Setoguchi <setomits@gmail.com>
2014-03-24 18:35:29 +0900

    Modified to fetch values of total entities

--- mow/handlers/admin.py
+++ mow/handlers/admin.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import g, render_template
 
 from mow import app
-from mow.models import User
+from mow.models import Comment, Entry, Tag, User
 from mow.handlers import login_required
 
 @app.route('/admin')
 @login_required
 def admin_top_page():
+    g.sqlalchemy_database_uri = app.config['SQLALCHEMY_DATABASE_URI']
+    g.n_total_comments = Comment.n_total_count()
+    g.n_total_entries = Entry.n_total_count()
+    g.n_total_tags = Tag.n_total_count()
+    g.n_total_users = User.n_total_count()
+
     return render_template('admin/top.html')
