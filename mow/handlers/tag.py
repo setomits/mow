bb36b6ee9befefe23c7e83df925681e61cb9bec8
Mitsuhiro Setoguchi <setomits@gmail.com>
2014-04-05 20:17:19 +0900

    Added handler to manage tags

--- mow/handlers/tag.py
+++ mow/handlers/tag.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from flask import g, redirect, render_template, url_for
+from flask import g, redirect, render_template, request, url_for
 
 from mow import app
 from mow.models import Entry, Tag
+from mow.handlers import login_required
 
 @app.route('/tag/<label>/')
 @app.route('/tag/<label>/<int:page>')
@@ -30,3 +31,20 @@ def tag_page(label, page = 1):
 @app.route('/tag/')
 def all_tags_page():
     return render_template('all_tags_page.html')
+
+
+@app.route('/admin/tag/edit', methods = ['GET', 'POST'])
+@login_required
+def tag_editing_page():
+    g.active = {'tag': True}
+
+    if request.method == 'POST':
+        current_label = request.form.get('current_label', '')
+        new_label = request.form.get('new_label', '').strip()
+
+        if Tag.n_entries(current_label):
+            Tag.update_label(current_label, new_label)
+
+
+    g.tag_labels = Tag.fetch_all_labels()
+    return render_template('admin/edit_tag_page.html')
