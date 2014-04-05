ef6f99033a8a1674d77053634873c64b431a6112
Mitsuhiro Setoguchi <setomits@gmail.com>
2014-04-05 17:32:28 +0900

    Added status parameter for admin tabs. Added handler for memcached.

--- mow/handlers/admin.py
+++ mow/handlers/admin.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 from os.path import getmtime, getsize
-from flask import g, render_template
+from flask import g, redirect, render_template, request, url_for
 
 from mow import app
 from mow.models import Comment, Entry, Tag, User
@@ -11,6 +11,7 @@ from mow.handlers import login_required
 @app.route('/admin/')
 @login_required
 def admin_top_page():
+    g.active = {'top': True}
     g.sqlalchemy_database_uri \
         = app.config['SQLALCHEMY_DATABASE_URI'].replace('sqlite:///', '')
     g.n_total_comments = Comment.n_total_count()
@@ -19,7 +20,7 @@ def admin_top_page():
     g.n_total_users = User.n_total_count()
     g.db_updated_at = datetime.fromtimestamp(
         getmtime(g.sqlalchemy_database_uri))
-    g.db_mbytes = getsize(g.sqlalchemy_database_uri) / 1024 / 1024
+    g.db_bytes = getsize(g.sqlalchemy_database_uri)
 
     g.mc_stats = {}
     for server, stats in g.mc.get_stats():
@@ -32,3 +33,34 @@ def admin_top_page():
         g.mc_stats[server.decode('utf-8')] = d
 
     return render_template('admin/top.html')
+
+
+@app.route('/admin/memcache', methods = ['GET', 'POST'])
+@login_required
+def admin_memcache_page():
+    g.active = {'memcache': True}
+    g.mc_key, g.mc_val = None, None
+
+    if request.method == 'POST':
+        mc_key = request.form.get('mc_key', '').strip()
+        if mc_key:
+            g.mc_key = mc_key
+            g.mc_val = g.mc.get(mc_key)
+
+    return render_template('admin/memcache.html')
+
+
+@app.route('/admin/memcache/clear', methods = ['POST'])
+@login_required
+def clear_memcache():
+    g.active = {'memcache': True}
+    g.mc_key, g.mc_val = None, None
+
+    mc_key = request.form.get('mc_key', '').strip()
+    if mc_key:
+        if mc_key == 'all':
+            g.mc.flush_all()
+        else:
+            g.mc.delete(mc_key)
+
+    return redirect(url_for('admin_memcache_page'))
